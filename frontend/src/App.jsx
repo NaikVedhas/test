81e112fe219b03b3f288edab746588c100b23b48
NaikVedhas <vedhasnaik121@gmail.com>
2025-01-28 19:57:54 +0530

    Frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [messageList,setMessageList] = useState([]);
+  const [message,setMessage] = useState("");
+
+  const handleClick = () =>{
+    
+    setMessage("");
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <input 
+      type="text"
+      value={message}
+      onChange={(e)=>setMessage(e.target.value)}
+      placeholder='Type your message'
+      />
+      <button onClick={handleClick}>Send Message</button>
+
+      <h1>All messageList</h1>
+      {messageList.length>0 ? (
+        <div>
+          {messageList.map((m,id) => (
+            <div key={id}>
+              {m}
+            </div>
+          ))}
+        </div>
+      ):(
+        <h3> No messageList present at the moment</h3>
+      )}
+    </div>
     </>
   )
 }
